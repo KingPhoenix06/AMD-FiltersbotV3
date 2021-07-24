2aef1418408b9b70aa97af33b5f6983a18fd6100
KingPhoenix06 <87867098+KingPhoenix06@users.noreply.github.com>
2021-07-24 16:06:51 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"♻️ Ꭻᴏɪɴ : @AMD_LinkZz ♻️   🗂️ Ꮇᴏᴠɪᴇ Ｎᴀᴍᴇ : <code>{query}</code>",
+                text=f"♻️ Ꭻᴏɪɴ : @AMD_LinkZz ♻️     🗂️ Ꮇᴏᴠɪᴇ Ｎᴀᴍᴇ : <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
